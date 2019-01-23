22086760e9a7fa62a3be3d4f04b6bdf8d42e89c2
almyk <hansentommy92@gmail.com>
2019-01-23 20:17:37 +0900

    Add textview to show month

--- app/src/main/java/com/example/almyk/sogangfootballfield/MainActivity.java
+++ app/src/main/java/com/example/almyk/sogangfootballfield/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.almyk.sogangfootballfield;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "MainActivity";
+    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy", Locale.getDefault());
+
+    private CompactCalendarView mCompactCalendarView;
+    private TextView mTitleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCompactCalendarView = findViewById(R.id.ccv_calendar);
+        mTitleTextView = findViewById(R.id.tv_title);
+
+        mTitleTextView.setText(dateFormatForMonth.format(mCompactCalendarView.getFirstDayOfCurrentMonth()));
+        mCompactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                mTitleTextView.setText(dateFormatForMonth.format(dateClicked));
+                Log.v(TAG, "Date clicked: " + dateClicked.toString());
+                Toast.makeText(getApplicationContext(), "Clicked on: " + dateClicked.toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                mTitleTextView.setText(dateFormatForMonth.format(firstDayOfNewMonth));
+            }
+        });
     }
 }
